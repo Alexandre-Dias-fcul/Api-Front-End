45dffe3d89b74aa9cdf4771f29e009709acbe8c2
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-05-01 15:04:30 +0100

    Inicio da elaboração da estrutura do back-office.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/back-office/login/login.component';
+import { HomeComponent } from './components/front-office/home/home.component';
+import { MainPageComponent } from './components/back-office/main-page/main-page.component';
+import { UserListComponent } from './components/back-office/user-list/user-list.component';
+import { UserNewComponent } from './components/back-office/user-new/user-new.component';
+import { UserEditComponent } from './components/back-office/user-edit/user-edit.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path:'main-page', component: MainPageComponent,children:
+        [
+          {path:'', redirectTo:'user-list', pathMatch:'full'},
+          {path:'user-list', component: UserListComponent},
+          {path:'user-new', component: UserNewComponent},
+          {path:'user-edit/:id', component: UserEditComponent}
+        ]}
+];
